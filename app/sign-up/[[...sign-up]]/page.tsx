455e47cf4628df1811a495bb3e774c3e48f0d23d
d3byn <d3bayansarkar@gmail.com>
2026-08-13 04:04:26 +0530

    fix(sign-in, sign-up): update background color for improved visual consistency

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <main style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', backgroundColor: '#09090b' }}>
+    <main style={{ display: 'flex', minHeight: '100vh', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0F0E47' }}>
       <SignUp fallbackRedirectUrl="/dashboard" />
     </main>
   );
